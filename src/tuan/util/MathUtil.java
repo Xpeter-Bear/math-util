bfb53860845117f0d15ad1b877f0d3b0b2027be6
Xpeter-Bear <nguyentuangk1308@gmail.com>
2020-11-19 08:34:06 +0700

    vetyfi

--- src/tuan/util/MathUtil.java
+++ src/tuan/util/MathUtil.java
@@ -10,25 +10,39 @@ package tuan.util;
  * @author Nam
  */
 public class MathUtil {
-    
+
     //class nay chuawas cac tien ich dung chung cho moi noi do do no hai nen la static
-    public static final double PI=3.1415;
+    public static final double PI = 3.1415;
     //ham tinh n giai thua n>-= ,111
     //neu qua 20 tran kieu long
     //0! = 1, 1! = 1, n!=1.2.3...n
-    public static long getFactorial(int n){
-        if (n < 0 || n > 20) 
-            throw  new IllegalArgumentException("n must be greater than 0 and less than 20");
+//    public static long getFactorial(int n){
+//        if (n < 0 || n > 20) 
+//            throw  new IllegalArgumentException("n must be greater than 0 and less than 20");
+//        //dua dau vao ca chiwn , minh khong them tra ve gias tri am chinsai rooi
+//        
+//        if ( n == 0 || n == 1) {
+//            return 1;//0! = 1! = 1
+//        }
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result = result * i;
+//        }
+//        return result;
+//    }
+
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("n must be greater than 0 and less than 20");
+        }
         //dua dau vao ca chiwn , minh khong them tra ve gias tri am chinsai rooi
-        
-        if ( n == 0 || n == 1) {
+
+        if (n == 0 || n == 1) {
             return 1;//0! = 1! = 1
         }
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result = result * i;
-        }
-        return result;
+        return n * getFactorial(n - 1);
+
     }
-    
+    // n! = n * (n - 1);
+
 }
